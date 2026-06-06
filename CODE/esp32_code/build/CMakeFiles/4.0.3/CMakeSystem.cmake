c4ea6bdc948c06f8bb342f438d2dfe307010c37e
Parasca Raul <raul.parasca@gmail.com>
2026-06-07 02:00:36 +0300

    Initializing app code (C++) and esp-32 code (C).

---
+++ CODE/esp32_code/build/CMakeFiles/4.0.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.26200")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.26200")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("R:/LICENTA/Sistem_mobil_cu_traseu_programabil/CODE/esp32_code/build/toolchain/toolchain-esp32.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ CODE/esp32_code/main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    while (1)
+    {
+        printf("Salut!\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
